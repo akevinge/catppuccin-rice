f2c89b97bb96fb464882ab87fac71c9b2212875e
samuelnihbos <partogusamuel07@gmail.com>
2022-01-14 12:33:50 +0700

    Change repository name, add some additional config

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -4,13 +4,9 @@ dunst &
 xsetroot -cursor_name left_ptr &
 xbacklight -set 30 &
 xinput set-prop 12 309 1 &
-polybar example -c ~/.config/polybar/config.ini &
 picom --config ~/.config/picom/picom.conf --experimental-backends &
-
-#nitrogen --restore &
-ncmpcpp &
 feh --bg-fill ~/.config/bspwm/wal.png
+#nitrogen --restore &
+polybar example -c ~/.config/polybar/config.ini &
+ncmpcpp &
 
-###Polkits, Uncomment the one that you using###
-#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/xfce-polkit/xfce-polkit &
